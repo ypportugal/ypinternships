6baef904bd0ca2280a63f0ac15940ace7d0bb3db
joaoq <joaoq@DESKTOP-C3J2S3N.home>
2015-10-03 15:29:51 +0100

    batatas

--- internships/src/main/java/ypportugal/internships/init/WebAppConfig.java
+++ internships/src/main/java/ypportugal/internships/init/WebAppConfig.java
@@ -21,18 +21,16 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 @Configuration
 @ComponentScan("ypportugal.internships")
 @EnableWebMvc
-@EnableTransactionManagement
-@PropertySource("classpath:application.properties")
 public class WebAppConfig {
      
-    private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
-    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password";
-    private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
-    private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
-     
-    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
-    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
-    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
+//    private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
+//    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password";
+//    private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
+//    private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
+//     
+//    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+//    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+//    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
      
     @Resource
     private Environment env;
